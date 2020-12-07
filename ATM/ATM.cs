7df181425b53cb313553ad3380a6930ceb07047e
AWilson <dreamy26@icloud.com>
2020-12-06 21:01:27 -0500

    added six methods for ATM.CS

--- ATM/ATM.cs
+++ ATM/ATM.cs
@@ -1,10 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ATM
 {
     public class ATM
     {
+        public List<Account> Accounts = new List<Account>();
+
         public ATM()
         {
+
         }
+
+        public string Register(string name, string password)
+        {
+            return "";
+        }
+
+        public string Login(string userName, string password)
+        {
+            return "";
+        }
+
+
+        public bool Logout()
+        {
+            return true;
+        }
+
+        public string CheckBalance()
+        {
+            Console.WriteLine("Ballance of account");
+            return "";
+           
+        }
+
+        public static void Deposit(int depositAmount)
+        {
+
+        }
+
+        public static void Withdraw()
+        {
+
+        }
+      
     }
 }
--- ATM/Account.cs
+++ ATM/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 namespace ATM
 {
+    // The account object will need to track the following, String Name, String Password, Int Balance
     public class Account
     {
         public Account()
